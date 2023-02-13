cc81e2129c39bfd63bde8e919eacba3830c4ac5e
adityab-dev <120619189+adityab-dev@users.noreply.github.com>
2023-02-13 12:14:31 +0530

    footer styled

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,66 +1,87 @@
 import "./Footer.css";
 
 import { FaTelegramPlane } from "react-icons/fa";
+import {
+  AiFillTwitterCircle,
+  AiFillFacebook,
+  AiFillInstagram,
+} from "react-icons/ai";
 
 function Footer() {
   return (
     <div className="footer-container">
-      <div className="footer-left">
-        <div>
-          <div>Contact</div>
-          <div>1800 900 32</div>
-          <div>1800 900 64</div>
+      <section className="footer-left">
+        <div className="footer-left-padding">
+          <section>
+            <div className="footer-left-headings">Contact</div>
+            <div>1800 900 32</div>
+            <div>1800 900 64</div>
+          </section>
+
+          <section>
+            <div className="footer-left-headings">Helpline Number</div>
+            <div>9090 1234 46</div>
+            <div>9090 1234 47</div>
+          </section>
+
+          <section>
+            <div className="footer-left-headings">Email:</div>
+            <div>complaint@electionIndia.gov.in</div>
+            <div>info@electionIndia.gov.in</div>
+          </section>
+        </div>
+      </section>
+
+      <hr />
+
+      <section className="footer-mid">
+        <div className="padding-top">
+          <section>
+            <div className="footer-left-headings">GetIN</div>
+            <div>Register</div>
+            <div>Login</div>
+          </section>
+
+          <section>
+            <div className="footer-left-headings">Know more</div>
+            <div>Features</div>
+            <div>About</div>
+            <div>Steps</div>
+          </section>
+
+          <section>
+            <div className="footer-left-headings">Follow us</div>
+            <div>Facebook</div>
+            <div>Instagram</div>
+            <div>Twitter</div>
+          </section>
         </div>
 
-        <div>
-          <div>Helpline Number</div>
-          <div>9090 1234 46</div>
-          <div>9090 1234 47</div>
-        </div>
+        <section>
+          <div className="footer-mid-imgs">
+            <AiFillTwitterCircle />
+            <AiFillFacebook />
+            <AiFillInstagram />
+          </div>
+        </section>
+      </section>
 
-        <div>
-          <div>Email:</div>
-          <div>complaint@electionIndia.gov.in</div>
-          <div>info@electionIndia.gov.in</div>
-        </div>
-      </div>
+      <hr />
 
-      <div className="footer-mid">
-        <div>
-          <div>GetIN</div>
-          <div>Register</div>
-          <div>Login</div>
-        </div>
-
-        <div>
-          <div>Know more</div>
-          <div>Features</div>
-          <div>About</div>
-          <div>Steps</div>
-        </div>
-
-        <div>
-          <div>Follow us</div>
-          <div>Facebook</div>
-          <div>Instagram</div>
-          <div>Twitter</div>
-        </div>
-      </div>
-
-      <div className="footer-right">
-        <div>Quick Feedback</div>
-        <input />
-        <textarea
-          name=""
-          id=""
-          cols={30}
-          rows={10}
-        ></textarea>
-        <button type="button">
-          <FaTelegramPlane />
-          Submit
-        </button>
-      </div>
+      <section className="footer-right">
+          <div className="footer-right-heading">Quick Feedback</div>
+          <input />
+          <textarea
+            name=""
+            id=""
+            cols={10}
+            rows={10}
+          ></textarea>
+          <button type="button">
+            <FaTelegramPlane />
+            Submit
+          </button>
+      </section>
     </div>
   );
 }
