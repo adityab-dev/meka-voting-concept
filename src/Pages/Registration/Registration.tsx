3aa754a3501a0614f298aca675b845abf2f0de3a
adityab-dev <120619189+adityab-dev@users.noreply.github.com>
2023-02-13 13:05:38 +0530

    registration styled

--- src/Pages/Registration/Registration.tsx
+++ src/Pages/Registration/Registration.tsx
@@ -1,3 +1,47 @@
+import "./Registration.css";
+
+import voting from "../../Assets/voting-img.jpg";
+
+import { inputNamesArray } from "../../constants/reg-input";
+
 export default function RegistrationPage() {
-  return <div>Registration Page</div>;
+  return (
+    <div className="reg-container">
+      <img
+        className="reg-img"
+        src={voting}
+        alt="Please Vote"
+      />
+
+      <section className="reg-form-container">
+        <h1 className="reg-heading">Registration Form</h1>
+
+        <form className="reg-form">
+          {inputNamesArray.map((name) => {
+            return (
+              <div className="reg-form-inputs-labels">
+                <label
+                  htmlFor={name}
+                  className="reg-labels"
+                >
+                  {name}
+                </label>
+                <input
+                  className="reg-inputs"
+                  type="text"
+                  id={name}
+                />
+              </div>
+            );
+          })}
+          <button
+            className="reg-form-btn"
+            type="submit"
+          >
+            SUBMIT
+          </button>
+        </form>
+      </section>
+    </div>
+  );
 }
---
+++ src/constants/reg-input.ts
@@ -0,0 +1,30 @@
+const enum inputNames {
+  NAME = "Name",
+  DOB = "Date Of Birth",
+  PARENT_NAME = "Father's/Mother's Name",
+  EMAIL = "Email",
+  MOBILE_NUM = "Mobile No.",
+  PASSWORD = "Password",
+  RE_PASSWORD = "Re-enter Password",
+  ADHAR_NUM = "Adhar Number",
+}
+
+export const NAME = inputNames.NAME;
+export const DOB = inputNames.DOB;
+export const PARENT_NAME = inputNames.PARENT_NAME;
+export const EMAIL = inputNames.EMAIL;
+export const MOBILE_NUM = inputNames.MOBILE_NUM;
+export const PASSWORD = inputNames.PASSWORD;
+export const RE_PASSWORD = inputNames.RE_PASSWORD;
+export const ADHAR_NUM = inputNames.ADHAR_NUM;
+
+export const inputNamesArray = [
+  NAME,
+  DOB,
+  PARENT_NAME,
+  EMAIL,
+  MOBILE_NUM,
+  PASSWORD,
+  RE_PASSWORD,
+  ADHAR_NUM,
+];
